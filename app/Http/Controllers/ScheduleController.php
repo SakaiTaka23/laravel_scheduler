43182b496a7e7b7b7596e848a1419f0c29c361c4
SakaiTaka23 <tt3.sakai@gmail.com>
2020-11-05 18:59:34 +0900

    サービスプロパイダを使わないように設定

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -5,86 +5,70 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
-use App\Service\ScheduleServiceInterface;
-
+use App\Models\Schedule;
 
 class ScheduleController extends Controller
 {
-    private $schedule;
-
-    public function __construct(
-        ScheduleServiceInterface $schedule
-    ) {
-        $this->schedule = $schedule;
-        $this->auth = Auth::id();
-    }
 
     public function index(Application $app)
     {
-        $schedules = $this->schedule->getUserSchedule($this->auth);
-        // $schedules = new Schedule();
-        // $schedules = $schedules->all();
+        $schedules = new Schedule();
+        $schedules = $schedules->all();
         return view("schedule/index", compact('schedules'));
     }
 
     public function create(Request $request)
     {
-        // $schedule = new Schedule();
-        // $schedule->place = $request['place'];
-        // $schedule->content = $request['content'];
-        // $schedule->begin = $request['begin'];
-        // $schedule->end = $request['end'];
-        // $schedule->user_id = Auth::id();
-        // $schedule->save();
-
-        $this->schedule->createSchedule($request['place'], $request['content'], $request['begin'], $request['end'], $this->auth);
+        $schedule = new Schedule();
+        $schedule->place = $request['place'];
+        $schedule->content = $request['content'];
+        $schedule->begin = $request['begin'];
+        $schedule->end = $request['end'];
+        $schedule->user_id = Auth::id();
+        $schedule->save();
 
         return redirect()->route("schedule.index");
     }
 
     public function update(Request $request, $schedule_id)
     {
-        // $schedule = new Schedule();
-        // $schedule = $schedule->where('id', $schedule_id)->first();
-        // $schedule->fill($request->all())->save();
-        $this->schedule->updateSchedule($schedule_id, $request['place'], $request['content'], $request['begin'], $request['end'], $this->auth);
+        $schedule = new Schedule();
+        $schedule = $schedule->where('id', $schedule_id)->first();
+        $schedule->fill($request->all())->save();
 
         return redirect()->route("schedule.index");
     }
 
     public function edit($schedule_id)
     {
-        // $schedule = new Schedule();
-        // $schedule = $schedule->where('id', $schedule_id)->first();
-        $this->schedule->ScheduleFromId($schedule_id);
+        $schedule = new Schedule();
+        $schedule = $schedule->where('id', $schedule_id)->first();
         return view("schedule/update", compact('schedule'));
     }
 
     public function destroy($schedule_id)
     {
-        // $schedule = new Schedule();
-        // $schedule = $schedule->where('id', $schedule_id);
-        // $schedule->delete();
-        $this->schedule->deleteSchedule($schedule_id);
+        $schedule = new Schedule();
+        $schedule = $schedule->where('id', $schedule_id);
+        $schedule->delete();
         return redirect()->route("schedule.index");
     }
 
     public function month()
     {
-        // $schedules = new Schedule();
-        // $schedules = $schedules->where('user_id', Auth::id())->where('begin', '>=', DB::raw('DATE_SUB(NOW(), INTERVAL 1 MONTH)'))
-        //     ->orWhere('end', '<=', DB::raw('DATE_SUB(NOW(), INTERVAL 1 MONTH)'))->get();
-        $schedules = $this->schedule->ScheduleMonth($this->auth);
+        $schedules = new Schedule();
+        $schedules = $schedules->where('user_id', Auth::id())->where('begin', '>=', DB::raw('DATE_SUB(NOW(), INTERVAL 1 MONTH)'))
+            ->orWhere('end', '<=', DB::raw('DATE_SUB(NOW(), INTERVAL 1 MONTH)'))->get();
         return view("schedule/index", compact('schedules'));
     }
 
     public function week()
     {
-        // $schedules = new Schedule();
-        // $schedules = $schedules->where('user_id', Auth::id())->where('begin', '>=', DB::raw('DATE_SUB(NOW(), INTERVAL 1 WEEK)'))
-        //     ->orWhere('end', '<=', DB::raw('DATE_SUB(NOW(), INTERVAL 1 MONTH)'))->get();
-        $schedules = $this->schedule->ScheduleWeek($this->auth);
+        $schedules = new Schedule();
+        $schedules = $schedules->where('user_id', Auth::id())->where('begin', '>=', DB::raw('DATE_SUB(NOW(), INTERVAL 1 WEEK)'))
+            ->orWhere('end', '<=', DB::raw('DATE_SUB(NOW(), INTERVAL 1 MONTH)'))->get();
         return view("schedule/index", compact('schedules'));
     }
 }
